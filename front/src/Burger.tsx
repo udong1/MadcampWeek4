944ee5e2cd6977f30bdb2c8e52991a82dffdc0a6
udong1 <dngus1082@gmail.com>
2024-01-20 17:23:49 +0900

    Fix : merge 오류 해결

--- front/src/Burger.tsx
+++ front/src/Burger.tsx
@@ -4,90 +4,81 @@ import * as Three from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import BurgerRecipe from './BurgerRecipe';
 import './App.css'
+import gsap from 'gsap';
 
 const Burger: React.FC = () => {
-  const burgerRef = useRef<HTMLDivElement>(null);
-  const burgerMesh = useRef<Three.Group | undefined>(undefined);
-  const exampleMesh = useRef<Three.Mesh | undefined>(undefined);
+    // const burgerMesh = useRef<Three.Group | undefined>(undefined);
+    const divRef = useRef<HTMLDivElement>(null)
+    const burgerGroupRef = useRef<Three.Group>(new Three.Group())
+    //scene
+    const scene = new Three.Scene();
+    scene.background = new Three.Color('white')
+    //light
+    const ambientLight = new Three.AmbientLight(0xffffff, 1);
+    scene.add(ambientLight)
+    const directionalLight = new Three.DirectionalLight(0xffffff,1)
+    directionalLight.position.set(10,10,0)
+    const targetObject = new Three.Object3D();
+    scene.add(targetObject);
+    targetObject.position.set(0, 0, 0); // 원하는 방향으로 조절
+    directionalLight.castShadow = true
+    directionalLight.shadow.mapSize.width = 1024;
+    directionalLight.shadow.mapSize.height = 1024;
+    scene.add(directionalLight)
+    //floor
+    const floorGeo = new Three.PlaneGeometry(10,10)
+    const floorMat = new Three.MeshStandardMaterial({ color : 0xeeeeee, roughness:0.8, metalness:0.1})
+    const floor = new Three.Mesh(floorGeo, floorMat)
+    floor.rotation.x = -Math.PI / 2
+    floor.position.set(0,-2,0)
+    floor.receiveShadow = true
+    scene.add(floor)
+    //camera
+    const camera = new Three.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0,0,5)
+    camera.lookAt(0,-1,1);
+    camera.lookAt(0,-1,0);
+
+    const renderer = new Three.WebGLRenderer({
+        antialias:true
+    });
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = Three.PCFSoftShadowMap;
+    renderer.shadowMap.autoUpdate = false;
+    renderer.toneMapping = Three.ACESFilmicToneMapping;
+
+
+
+
+
+
+
   useEffect(() => {
-    if (burgerRef.current) {
+    console.log("useEffect")
+    //burger 생성
+    const loader = new GLTFLoader();
+    function loadBurger(){
+        for (const ingredient of BurgerRecipe){
+            console.log(ingredient)
+                loader.load(process.env.PUBLIC_URL+`/stylized_burger/${ingredient}.glb`, (gltf)=>{
+                    const temp = gltf.scene
+                    temp.position.set(0,-1.5,0)
+                    temp.receiveShadow=true
+                    temp.castShadow=true
+                    burgerGroupRef.current.add(temp)
+            })   
+        }
+        scene.add(burgerGroupRef.current)
+        console.log("scene add")
+    }
+    
+    loadBurger()
 
-        const clock = new Three.Clock();
-        const scene = new Three.Scene();
-        scene.background = new Three.Color('green');
 
-        const camera = new Three.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.set(0, 5, 8);
-        camera.lookAt(0, 0, 0);
+    if (divRef.current) {
 
-        const renderer = new Three.WebGLRenderer({
-            antialias: true,
-            });
-
-        const ambientLight = new Three.AmbientLight(0xffffff, 1);
-        scene.add(ambientLight)
-
-        const directionalLight = new Three.DirectionalLight(0xffffff, 1);
-        directionalLight.position.set(10, 10, 0);
-        const targetObject = new Three.Object3D();
-        targetObject.position.set(0, 0, 0); // 원하는 방향으로 조절
-        scene.add(targetObject);
-        directionalLight.target = targetObject;
-        directionalLight.castShadow = true;
-        directionalLight.shadow.mapSize.width = 1024;
-        directionalLight.shadow.mapSize.height = 1024;
-        scene.add(directionalLight);
-
-        renderer.shadowMap.enabled = true;
-        renderer.shadowMap.type = Three.PCFSoftShadowMap;
-
-        burgerRef.current.appendChild(renderer.domElement);
-
-        const loader = new GLTFLoader();
-
-        loader.load('./stylized_burger/colorTest.glb', (gltf) => {
-            const burger = gltf.scene;
-            burger.position.set(0, 1.5, 0);
-            burger.receiveShadow = true;
-            burger.traverse(function(node) {
-                if (node instanceof Three.Mesh) {
-                    const originalMaterial = node.material;
-
-                    const color = originalMaterial.color;
-                    console.log('Color:', color);
-
-                    const standardMaterial = new Three.MeshStandardMaterial({ color });
-
-                    node.material = standardMaterial;
-                    
-                    node.castShadow = true;
-                    node.receiveShadow = true;
-                }
-            });
-            burgerMesh.current = burger;
-            scene.add(burger);
-        });
-
-        // const ex = new Three.BoxGeometry(1, 1, 1);
-        // const exMaterial = new Three.MeshStandardMaterial({ color: 0x00ff00 });
-        // const example = new Three.Mesh(ex, exMaterial);
-        // example.position.set(1, 0, 0);
-        // exampleMesh.current = example;
-        // example.receiveShadow = true; 
-        // example.castShadow = true; 
-        // scene.add(example);
-
-        const planeGeometry = new Three.PlaneGeometry(20, 20, 1, 1);
-        const planeMaterial = new Three.MeshStandardMaterial({
-            color: 0xffffff,
-            roughness: 0.8,
-            metalness: 0.2,
-        });
-        const plane = new Three.Mesh(planeGeometry, planeMaterial);
-        plane.rotation.x = -Math.PI / 2;
-        plane.position.y = 0;
-        plane.receiveShadow = true; 
-        scene.add(plane);
+   
+        divRef.current.appendChild(renderer.domElement)
 
         const handleResize = () => {
             const newWidth = window.innerWidth;
@@ -99,33 +90,64 @@ const Burger: React.FC = () => {
             renderer.setSize(newWidth, newHeight);
         };
 
-        const rotationSpeed = 0.5;
-        const animate = () => {
-            const delta = clock.getDelta();
-            requestAnimationFrame(animate);
-        
-            if (burgerMesh.current) {
-            burgerMesh.current.rotation.y += rotationSpeed * delta;
-            }
-            if (exampleMesh.current) {
-                exampleMesh.current.rotation.y += rotationSpeed * delta;
-            }      
-            renderer.render(scene, camera);
-        };
-
         handleResize();
-        animate();
+        startAnimate();
 
         window.addEventListener('resize', handleResize);
 
         return () => {
             window.removeEventListener('resize', handleResize);
             renderer.dispose();
-        };
-    }
-  }, []);
+        }}
+    }, []);
 
-  return <div ref={burgerRef}></div>;
+    const startAnimate = () =>{
+        requestAnimationFrame(startAnimate)
+        burgerGroupRef.current.rotation.y += 0.001;
+        renderer.render(scene, camera)
+    }
+    function openBurger(){
+        console.log("openBurger")
+        openBurgerAnimate()
+    }
+    function closeBurger(){
+        console.log("closeBurger")
+        closeBurgerAnimate()
+    }
+
+    const openBurgerAnimate = () =>{
+        const bunBottom = burgerGroupRef.current.getObjectByName("bun_bottom")
+        const bunCheese = burgerGroupRef.current.getObjectByName("bun_cheese")
+        const onion = burgerGroupRef.current.getObjectByName("onion")
+        gsap.to(bunBottom!!.position,{
+            duration: 0.3,
+            y:-0.25,
+        })
+        gsap.to(bunCheese!!.position, {
+            duration: 0.3,
+            y:-0.25,
+        })
+        gsap.to(onion!!.position,{
+            duration: 0.3,
+            y:-0.25,
+        })
+    }
+    const closeBurgerAnimate = () => {
+        const bunBottom = burgerGroupRef.current.getObjectByName("bun_bottom")
+        gsap.to(bunBottom!!.position,{
+            duration: 0.3,
+            y:0.04,
+        })
+    }
+
+
+    return (
+    <div ref={divRef}>
+        <div className="Button_container">
+            <button className="Start" onMouseEnter={openBurger} onMouseLeave={closeBurger}>START</button>
+        </div>
+    </div>
+    );
 };
 
 export default Burger;
