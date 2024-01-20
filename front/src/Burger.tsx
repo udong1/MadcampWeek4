9fa933d4eb8ce48672da84a90f0c5178ba405f58
udong1 <dngus1082@gmail.com>
2024-01-20 10:26:22 +0900

    WIP : 그림자 작업중

--- front/src/Burger.tsx
+++ front/src/Burger.tsx
@@ -17,13 +17,29 @@ const Burger : React.FC=()=>{
             const scene = new Three.Scene();
             scene.background = new Three.Color('white')
 
+            const light = new Three.DirectionalLight(0xffffff,1)
+            light.position.set(-30,40,10)
+            light.castShadow = true
+            scene.add(light)
+
+            const floorGeo = new Three.PlaneGeometry(10,10)
+            const floorMat = new Three.MeshStandardMaterial({ color : 0xeeeeee, roughness:0.8, metalness:0.1})
+            const floor = new Three.Mesh(floorGeo, floorMat)
+            floor.rotation.x = -Math.PI / 2
+            floor.position.set(0,-2,0)
+            floor.receiveShadow = true
+            scene.add(floor)
+
             const camera = new Three.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
-            camera.position.set(0,-0.5,5);
+            camera.position.set(0,0,5)
+            camera.lookAt(0,-1,1);
 
             const renderer = new Three.WebGLRenderer({
                 antialias:true
             });
             renderer.toneMapping = Three.ACESFilmicToneMapping;
+            renderer.shadowMap.enabled = true
+            renderer.shadowMap.type = Three.PCFShadowMap
 
 
             burgerRef.current.appendChild(renderer.domElement)
@@ -33,9 +49,11 @@ const Burger : React.FC=()=>{
             loader.load(process.env.PUBLIC_URL+"/stylized_burger/scene.gltf", (gltf)=>{
                 const burger = gltf.scene
                 burger.position.x=0
-                burger.position.y=0.3
+                burger.position.y=-0.5
                 burger.position.z=0
                 console.log("burger", burger.position)
+                burger.receiveShadow=true
+                burger.castShadow=true 
                 scene.add(burger)
             })
             
