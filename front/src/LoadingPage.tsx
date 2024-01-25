ea238103843a9951d18d772aa2caef773a9a2c4c
udong1 <dngus1082@gmail.com>
2024-01-25 16:11:18 +0900

    feat : loadingpage 추가

--- front/src/LoadingPage.tsx
+++ front/src/LoadingPage.tsx
@@ -1,8 +1,9 @@
 import { useNavigate } from "react-router-dom"
 import './LoadingPage.css'
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules'
+import gsap from 'gsap'
 import 'swiper/css';
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
@@ -11,6 +12,7 @@ import 'swiper/css/pagination'
 function LoadingPage(){
     const navigate = useNavigate()
     const [userNickname, setUserNickname] = useState<string>("")
+    const hambergerRef = useRef<HTMLImageElement | null>(null)
 
     function moveToMain(){
         navigate('/main')
@@ -25,9 +27,23 @@ function LoadingPage(){
         }
 
     }
+
     useEffect(()=>{
         getNickname()
     })
+    useEffect(()=>{
+        if(hambergerRef.current){
+            console.log("start animation")
+            gsap.to(hambergerRef.current, {
+                duration : 1,
+                repeat : -1,
+                yoyo : true,
+                rotation : 10,
+                ease : "none",
+            })
+        }
+    },[hambergerRef])
+    
 
     return (
         <div className="Loading_page">
@@ -56,7 +72,7 @@ function LoadingPage(){
                         <div>
                             <img className="Onboarding_img"src={process.env.PUBLIC_URL+"/onboarding/onboarding2.png"} alt=""/>
                             <p className="Onboarding_description">
-                                점수를 확인하세요!
+                                점수를 확인하세요
                             </p>
                         </div>
                     </SwiperSlide>
@@ -65,7 +81,7 @@ function LoadingPage(){
                             <img className="Onboarding_img"src={process.env.PUBLIC_URL+"/onboarding/onboarding3.png"} alt=""/>
                             <p className="Onboarding_description">
                                 남은 시간을 확인하고<br/>
-                                주어진 순서대로 재료를 쌓으세요
+                                순서대로 재료를 쌓아요
                             </p>
                         </div>
                     </SwiperSlide>
@@ -73,8 +89,17 @@ function LoadingPage(){
                         <div>
                             <img className="Onboarding_img"src={process.env.PUBLIC_URL+"/onboarding/onboarding4.png"} alt=""/>
                             <p className="Onboarding_description">
-                                잘못 만든 버거는 휴지통을 눌러 버려주세요
+                                잘못 만든 버거는 꼭 휴지통을 눌러 버려주세요!
                             </p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div>
+                            <img className="Onboarding_img"src={process.env.PUBLIC_URL+"/onboarding/onboarding5.png"} alt=""/>
+                            <p className="Onboarding_description">
+                                START를 눌러 시작!
+                            </p>
+                            <img ref={hambergerRef} className="Onboarding_hamburger"src={process.env.PUBLIC_URL+"/onboarding/hamburger.png"} alt=""/>
                         </div>
                     </SwiperSlide>
                 </Swiper>
